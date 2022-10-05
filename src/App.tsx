54f67ecc10e2ca3cf51d22bca6fb97e14aad3da4
bladetyr <bladetyr@udel.edu>
2022-10-05 10:41:01 -0400

    test change to make sure I fixed the conflict

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import { Counter } from "./components/Counter";
 
 function App(): JSX.Element {
     return (
+        //test change
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
