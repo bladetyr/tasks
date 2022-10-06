6be1829502cd73f74e6a460d63b742c9c2ba55ec
bladetyr <bladetyr@udel.edu>
2022-10-06 12:16:16 -0400

    TwoDie For

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,54 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attempts, setAttempts] = useState<number>(4);
+    const [disable, setDisable] = useState<boolean>(true);
+    const [inProgress, setProgress] = useState<boolean>(true);
+
+    function disableToggle(): void {
+        setDisable(!disable);
+    }
+
+    function progressToggle(): void {
+        setProgress(!inProgress);
+    }
+
+    return (
+        <div>
+            <div>
+                <Button
+                    onClick={() => setAttempts(1 + attempts)}
+                    disabled={!disable}
+                >
+                    Mulligan
+                </Button>
+                Your Attempts: {attempts}
+            </div>
+
+            <div>
+                <Button
+                    onClick={() => {
+                        progressToggle();
+                        disableToggle();
+                        setAttempts(attempts - 1);
+                    }}
+                    disabled={!disable || attempts === 0}
+                >
+                    Start Quiz
+                </Button>
+            </div>
+
+            <div>
+                <Button
+                    onClick={() => {
+                        progressToggle();
+                        disableToggle();
+                    }}
+                    disabled={disable}
+                >
+                    Stop Quiz
+                </Button>
+            </div>
+        </div>
+    );
 }
